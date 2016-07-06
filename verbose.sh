33e112764eccf8bdcc6d8583e0f2c2d794c32d62
Dan Wallach <dwallach@cs.rice.edu>
2016-07-05 22:32:03 -0700

    script to verbosify all the things

---
+++ verbose.sh
@@ -0,0 +1,10 @@
+adb shell setprop log.tag.Constants VERBOSE
+adb shell setprop log.tag.NotificationHelper VERBOSE
+adb shell setprop log.tag.NotificationService VERBOSE
+adb shell setprop log.tag.Receiver VERBOSE
+adb shell setprop log.tag.SharedState VERBOSE
+adb shell setprop log.tag.StopwatchProviderServic VERBOSE
+adb shell setprop log.tag.StopwatchState VERBOSE
+adb shell setprop log.tag.TimerProviderService VERBOSE
+adb shell setprop log.tag.TimerState VERBOSE
+adb shell setprop log.tag.Utils VERBOSE
